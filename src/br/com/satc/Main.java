1d6f13bf31919696304f989d52fd51d3a19de44f
gabriela.154717 <gabriela.154717@L-1-17-06--02.satc.edu.br>
2018-03-16 16:39:26 -0300

    Adicionado a Classe Pessoa,Disciplina,Aluno e Prof

--- src/br/com/satc/Main.java
+++ src/br/com/satc/Main.java
@@ -43,31 +43,7 @@ public class Main {
             disciplinasDisponiveis += "\n" + materias.lastIndexOf(materia) + " " + materia.getNome();
         }
 
-        List<Pessoa> pessoas = new ArrayList<>();
-        do {
-            String nome, rg, cpf;
-            Date dataNascimento;
-            SimpleDateFormat conversor = new SimpleDateFormat("dd/MM/yyyy");
-
-            nome = JOptionPane.showInputDialog("Informe seu Nome: ");
-            rg = JOptionPane.showInputDialog("Informe seu RG: ");
-            cpf = JOptionPane.showInputDialog("Informe seu CPF: ");
-            dataNascimento = conversor.parse(JOptionPane.showInputDialog("informe a Data de Nascimento: "));
-            pessoas.add(new Pessoa(nome, rg, cpf, dataNascimento) {
-            });
-
-        } while ((JOptionPane.showConfirmDialog(null, "Deseja Continuar?")) == 0);
-        String pessoasCadastradas = "";
-        for (Pessoa cadastro : pessoas) {
-            pessoasCadastradas += "\n" + pessoas.lastIndexOf(cadastro) + " "
-                    + "\n" + cadastro.getNome() + " "
-                    + "\n" + cadastro.getRg() + " "
-                    + "\n" + cadastro.getCpf() + " "
-                    + "\n" + cadastro.getIdade() + " Anos";
-        }
-
-        JOptionPane.showMessageDialog(null, disciplinasDisponiveis);
-        JOptionPane.showMessageDialog(null, pessoasCadastradas);
+       
     }
 
 }
